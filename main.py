89becf95a30fb07d2c2aa82d4871afd93327752c
Aman <ab3199bansal@gmail.com>
2023-11-10 18:54:56 +0530

    Perform a PUT-Request in Postman (Change the movie Details of ID )

--- main.py
+++ main.py
@@ -58,6 +58,51 @@ def get_movies():
     #     return {"message":"invalid response"}
 
 
+@app.route("/movies/<int:id>", methods=["PUT"])
+def movie_update(id):
+    data = request.json
+
+    name = data.get("name")
+
+    genre = data.get("genre")
+
+    Release_date = data.get("Release_date")
+
+    if name is not None and genre is not None and Release_date is not None:
+        movie = Movie.query.get(id)
+        if movie:
+            try:
+                movie.name = name
+                movie.genre =genre
+                movie.Release_date = Release_date
+                db.session.commit()
+
+                return {"message": "Movie Updated Successfully","id": movie.id}, 200
+            except IntegrityError:
+                db.session.rollback()
+                return {"message": "Movie Update failed"},400
+        else:
+            return {"message": "Movie not found"},404
+    else:
+        return {"message": "invalid data"}, 400
+
+@app.route("/movies/<int:id>", methods=["DELETE"])
+def Delete_Record(id):
+
+    try:
+        with app.app_context():
+            movie = Movie.query.get(id)
+            print(movie.name)
+            if movie:
+                db.session.delete(movie)
+                db.session.commit()
+                return {"message": "Movie deleted succesfully"},200
+            else:
+                return {"message": "Movie not found"},404
+    except IntegrityError:
+        db.session.rollback()
+        return {"message": "movie deletion failed"},500
+
 
 if __name__ == "__main__":
     app.run(debug=True)
